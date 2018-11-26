64fa1a5113e96ecfb7bc820ceeaa952ee6a6e89d
ody <ody@infochange>
2018-11-26 11:49:42 +0800

    move org.apache.common.io.*

---
+++ semantic.DA/src/test/java/io/odysz/semantic/DA/ConnectsTest.java
@@ -0,0 +1,21 @@
+package io.odysz.semantic.DA;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConnectsTest {
+
+	@Test
+	void testInit() {
+	}
+
+	@Test
+	void testGenId() {
+	}
+
+	@Test
+	void testSelect() {
+	}
+
+}
