430ae20602bda8de165be411ae449cb9594c8095
Odys Zhou <odys.zhou@gmail.com>
2023-04-08 23:39:51 +0800

    .

--- semantic.DA/src/test/java/io/odysz/semantic/DBSyntextTest.java
+++ semantic.DA/src/test/java/io/odysz/semantic/DBSyntextTest.java
@@ -31,18 +31,17 @@ import io.odysz.transact.sql.parts.condition.Funcall;
 import io.odysz.transact.x.TransException;
 
 class DBSyntextTest {
-	public static final String conn0 = "synode-main";
-	public static final String conn1 = "synode-s1";
-	public static final String conn2 = "synode-s2";
-	public static final String conn3 = "synode-s3";
+	public static final String conn0 = "synode-0";
+	public static final String conn1 = "synode-1";
+	public static final String conn2 = "synode-2";
+	public static final String conn3 = "synode-3";
 	public static final String rtroot = "src/test/res/";
 
-	public static final String father = "/test/res/Sun Yet-sen.jpg";
-	public static final String s0 = "s0";
-	public static final String s1 = "s1";
-	public static final String s2 = "s2";
-	public static final String s3 = "s3";
-
+	public static final String father = "src/test/res/Sun Yet-sen.jpg";
+	public static final int X = 0;
+	public static final int Y = 1;
+	public static final int Z = 2;
+	public static final int W = 3;
 	static String runtimepath;
 
 	static DBSynsactBuilder trb0;
@@ -58,6 +57,11 @@ class DBSyntextTest {
 	static SynChangeMeta chm;
 	static SynSubsMeta sbm;
 	static SynCleanMeta clnm;
+
+	static Ck c0;
+	static Ck c1;
+	static Ck c2;
+	static Ck c3;
 
 	static {
 		try {
@@ -145,90 +149,188 @@ class DBSyntextTest {
 		Connects.commit(conn1, robot, sqls, Connects.flag_nothing);
 		Connects.commit(conn2, robot, sqls, Connects.flag_nothing);
 		Connects.commit(conn3, robot, sqls, Connects.flag_nothing);
-	}
-
-	@Test
-	void testChangeLog() throws TransException, SQLException {
-		Ck c0 = new Ck(conn0);
-		Ck c1 = new Ck(conn1);
-		Ck c2 = new Ck(conn2);
-		Ck c3 = new Ck(conn3);
-
-		// 1. insert h_photos
-		String pid = insertPhoto0();
 		
-		// 2.1. check syn_change added CRUD.C
-		c0.changeC(pid);
-		// 2.2. check syn_subscribe added s1, s2, s3
-		c0.subs(pid, null, CRUD.C, C, C);
-
-		// 3. udpate h_photos
-		updatePoto0(pid);
 		
-		// 4. check CRUD.C over ride CRUD.U
-		c0.changeC(pid);
-		c0.subs_CCC(pid);
-		
-		// 5. s1 pull
-		s1pull(pid);
-		
-		// 6. update h_photos
-		updatePoto0(pid);
-
-		// 7. check CRUD.C over ride CRUD.U
-		c0.changeU(pid);
-		c0.subs(pid, null, C, null, C);
-
-		// 8. s2 pull
-		String s2pid = s2pull();
-		c0.changeU(pid);
-		c0.subs(pid, null, U, null, C);
-		c2.changeU(s2pid);
-		c2.subs(s2pid, null, U, null, C);
-
-		// 9. s1, s3 pull
-		s1pull();
-		c1.changeX(pid);
-
-		s3pull();
-		c3.changeX(pid);
-
-		c0.changeX(pid);
-
-		// s0: null, s1: null, s2: null, s3: null
-		c0.subs(pid, null, null, null, null);
-
-		// 10. s1 delete m1:clientpath
-		s1delm1f();
-		pid = s1push("m1", father);
-		c0.changeD(pid);
-		c0.clean(pid, null, null, D, D);
-	}
-
-	String s1push(String synode, String clientpath) {
-		return null;
+		c0 = new Ck(conn0);
+		c1 = new Ck(conn1);
+		c2 = new Ck(conn2);
+		c3 = new Ck(conn3);
 	}
 
 	/**
-	 * s1 delete s1:father
+	 * <pre>
+	 *    a b c
+	 *  A 0 0 0
+	 *  B 0 0 0
+	 *  C 0 0 0
+	 * 
+	 * A                                       | B
+	 * crud, synode, pid, nyquence, subscribe  | crud, synode, pid, nyquence, subscribe
+	 *  I,   A,      pA0, 0,         B         |  I,   B,      pB0, 0,         A 
+	 *                               C         |                               C 
+	 *                               D         |                               D 
+	 * 
+	 *    a b c
+	 *  A 1 0 0
+	 *  B 0 1 0
+	 *  C 0 0 0
+	 *  
+	 * A                                        =) B
+	 * crud, synode, pid, nyquence, subscribe   |  crud, synode, pid, nyquence, subscribe
+	 *  I,   A,      pA0, 1,        [ ]         |   I,    B,     pB0,  1,        A 
+	 *                               C          |                                C 
+	 *                               D          |                                D 
+	 *                                          |   I,    A,     pA0,  1,        C 
+	 *                                          |                                D 
+	 *                                          |   (B.a = A.a)
+	 *
+	 *    a b c
+	 *  A 1 0 0
+	 *  B 1 1 0
+	 *  C 0 0 0
+	 *  
+	 * A                                        =) B
+	 * crud, synode, pid, nyquence, subscribe   |  crud, synode, pid, nyquence, subscribe
+	 *  I,    A,     pA0, 1,        [ ]         |   I,   B,      pB0, 1,        [ ]
+	 *                               C          |                                C 
+	 *                               D          |                                D 
+	 *  I,    B,     pB0, 1,         C          |   I,   A,      pA0, 1,         C 
+	 *                               D          |                                D 
+	 *  (A.b = B.b)
+	 *
+	 *    a b c
+	 *  A 1 1 0
+	 *  B 1 1 0
+	 *  C 0 0 0
+	 * </pre>
+	 * @throws TransException
+	 * @throws SQLException
 	 */
-	void s1delm1f() {
+	@Test
+	void test01InsertBasic() throws TransException, SQLException {
+
+		// 1. insert A
+		String pA0 = insertPhotoA();
+		
+		// 1.1. syn_change.curd = C
+		c0.changeC(pA0);
+		// 1.2. syn_subscribe.to = [B, C, D]
+		c0.subs(pA0, null, C, C, C);
+
+		// 2. insert B
+		String pB0 = insertPhotoA();
+
+		// 1.1. syn_change.curd = C
+		c1.changeC(pB0);
+		// 1.2. syn_subscribe.to = [A, C, D]
+		c1.subs(pB0, C, null, C, C);
+		
+		// 3.
+		B_pullA();
+		// 3.1. syn_change.curd = C
+		c1.changeC(pA0);
+		// 3.2. syn_subscribe.to = [C, D]
+		c1.subs(pA0, null, C, C, C);
+		// 3.3. B.a = A.a
+		int Aa = c0.nyquence(X);
+		assertEquals(Aa, c1.nyquence(X));
+		
+		// 4.
+		B_pushA();
+		// 4.1. syn_change.curd = C
+		c1.changeC(pA0);
+		// 4.2. syn_subscribe.to = [C, D]
+		c1.subs(pA0, null, C, C, C);
+		// 4.3. A.b = B.b
+		int Bb = c1.nyquence(Y);
+		assertEquals(Bb, c0.nyquence(Y));
 	}
 
-	void s3pull() {
+	/**
+	 * <pre>
+	 * A                     | B                    | C                    | D
+	 * crud, s, pid, n, sub  | crud, s, pid, n, sub | crud, s, pid, n, sub | crud, s, pid, n, sub
+	 *  I,   A, pA0, 1, [ ]  |  I,   B, pB0, 1, [ ] |                      | 
+	 *                   C   |                   C  |                      |
+	 *                   D   |                   D  |                      |
+	 *  I,   B, pB0, 1,  C   |  I,   A, pA0, 1,  C  |                      |
+	 *                   D   |                   D  |                      |
+	 *                         [B.a = A.a]
+	 *    a b c d
+	 *  A 1 1 0 0
+	 *  B 1 1 0 0
+	 *  C 0 0 0 0
+	 *  D 0 0 0 0
+	 *  
+	 * B vs. C: B.n=1 > C.b, B =) C
+	 * A                     | B                   =) C                    | D
+	 * crud, s, pid, n, sub  | crud, s, pid, n, sub | crud, s, pid, n, sub | crud, s, pid, n, sub
+	 *  I,   A, pA0, 1, [ ]  |  I,   B, pB0, 1, [ ] |  I,   B, pB0, 1,     |
+	 *                   C   |                  [-] |                  [ ] |
+	 *                   D   |                   D  |                   D  |
+	 *  I,   B, pB0, 1,  C   |  I,   A, pA0, 1,  C  |  I,   A, pA0, 1,     |
+	 *                   D   |                   D  |                   D  |
+	 *                                                [C.a = max(B.a, C.a), C.b = B.b, C.c, C.d = max(B.d, C.d)]
+	 *    a b c d
+	 *  A 1 1 0 0
+	 *  B 1 1 0 0
+	 *  C 1 1 0 0
+	 *  D 0 0 0 0
+	 *  
+	 * A update pA0
+	 * A
+	 * crud, s, pid, n, sub  |
+	 *  U,   A, pA0, 2,  B   |
+	 *                   C   |
+	 *                   D   |
+	 *  I,   B, pB0, 1,  C   |
+	 *                   D   |
+	 *    a b c d
+	 *  A 2 1 0 0
+	 *  B 1 1 0 0
+	 *  C 1 1 0 0
+	 *  D 0 0 0 0
+	 * 
+	 * A                                                                  A=)D
+	 * crud, s, pid, n, sub  |                                             | crud, s, pid, n, sub
+	 *  U,   A, pA0, 2,  B   |                                             |  U,   A, pA0, 2,  B   (2>D.b=0)
+	 *                   C   |                                             |                   C   (2>D.c=0)
+	 *                  [ ]  |                                             |
+	 *  I,   B, pB0, 1,  C   |                                             |  I,   B, pB0, 1,  C   (1>D.c=0)
+	 *                  [ ]  |                                             |
+	 *                                                                       [D.a=A.a, D.b=A.b]
+	 *    a b c d
+	 *  A 2 1 0 0
+	 *  B 1 1 0 0
+	 *  C 1 1 0 0
+	 *  D 2 1 0 0
+	 *
+	 * B update pB0, C insert pC0
+	 * A                     | B                    | C                    | D
+	 * crud, s, pid, n, sub  | crud, s, pid, n, sub | crud, s, pid, n, sub | crud, s, pid, n, sub
+	 *                       |  U,   B, pB0, 2,  A  |  I,   B, pB0, 1,     | 
+	 *                       |                   C  |                  [ ] |
+	 *                       |                   D  |                   D  |
+	 *                       |  I,   A, pA0, 1,  C  |  I,   B, pA0, 1,     |
+	 *                       |                   D  |                   D  |
+	 *                       |                      |  I,   C, pC0, 1,  A  |
+	 *                       |                      |                   B  |
+	 *                       |                      |                   D  |
+	 *                       
+	 * B update pB0, C insert pC0, A vs. C, B vs. D
+	 * </pre>
+	 * @throws TransException
+	 * @throws SQLException
+	 */
+	void test02UpdateTransit() throws TransException, SQLException {
 	}
-
-	String s2pull() {
-		return null;
-	}
-
-	void s1pull() {
-	}
-
-	void s1pull(String pid) throws TransException {
+	
+	void B_pullA() throws TransException {
 		((DBSyntext) trb1.instancontxt(conn1, robot))
 			.synPull(phm);
 	}
+
+	void B_pushA() { }
 
 	void updatePoto0(String pid) throws TransException, SQLException {
 		trb0.update(phm.tbl, robot)
@@ -238,7 +340,7 @@ class DBSyntextTest {
 			;
 	}
 
-	String insertPhoto0() throws TransException, SQLException {
+	String insertPhotoA() throws TransException, SQLException {
 		return ((SemanticObject) trb0
 			.insert(phm.tbl, robot)
 			// .nv(phm.uri, "")
@@ -265,17 +367,9 @@ class DBSyntextTest {
 			
 			chg.next();
 			
-			assertEquals(CRUD.C, chg.getString(chm.crud));
+			assertEquals(C, chg.getString(chm.crud));
 			assertEquals(phm.tbl, chg.getString(chm.recTabl));
 			assertEquals(robot.deviceId(), chg.getString(chm.synoder));
-		}
-
-		/**
-		 * h_photos[pid].crud = D, syn_change[pid].crud = D
-		 * 
-		 * @param pid
-		 */
-		public void changeD(String pid) {
 		}
 
 		public void subs_CCC(String pid) throws TransException, SQLException {
@@ -299,24 +393,6 @@ class DBSyntextTest {
 					 assertIn("s1", s)));
 		}
 
-		/**
-		 * Verify pid's change log is as updated.
-		 * @param pid
-		 */
-		public void changeU(String pid) {
-		}
-
-		public void changeX(String pid) {
-		}
-
-		/**
-		 * Verify pid's subscriptions are U, C, C.
-		 * @param pid
-		public void subs_UCC(String pid) { }
-
-		public void subs_U_C(String pid) { }
-		 */
-
 		/**verify subscriptions.
 		 * @param pid
 		 * @param s subscriptions for s0/s1/s2/s3
@@ -328,6 +404,10 @@ class DBSyntextTest {
 		 * @param pid
 		 */
 		public void clean(String pid, String ... s) { }
+		
+		public int nyquence(int synode) {
+			return 0;
+		}
 	}
 
 }
