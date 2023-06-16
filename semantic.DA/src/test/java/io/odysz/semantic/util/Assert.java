294a89187fca932a11c62655f4df02badfef7cfa
odys zhou <odysseusj@163.com>
2023-06-16 16:47:42 +0800

    .

--- semantic.DA/src/test/java/io/odysz/semantic/util/Assert.java
+++ semantic.DA/src/test/java/io/odysz/semantic/util/Assert.java
@@ -1,13 +1,13 @@
 package io.odysz.semantic.util;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
 public class Assert {
 	
-	public static <T extends Object> HashSet<T> assertIn(T id, T ... ids) {
+	public static <T extends Object> HashSet<T> assertIn(T id, T ids) {
 		HashSet<T> s = new HashSet<T>(Arrays.asList(ids));
 		assertTrue(s.contains(id));
 		return s;
