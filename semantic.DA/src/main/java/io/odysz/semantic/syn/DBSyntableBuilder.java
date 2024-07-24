24f89ee1ef8cce1018112a970c80a78268961d30
Ody Zelenskyy <odys.zhou@gmail.com>
2024-07-24 17:55:42 +0800

    debug memo:
    It's not the complexity of lazy instantiation, it's the design error of
    DBSyntableBuilder.stamp wich leads to the puzzle of creating Syntier.doctrb.
    That is, if creating an instance of DBSyntableBuilder without a domain, the
    stamp can not intialized, which is used for creating doc-entity for any domain.

--- semantic.DA/src/main/java/io/odysz/semantic/syn/DBSyntableBuilder.java
+++ semantic.DA/src/main/java/io/odysz/semantic/syn/DBSyntableBuilder.java
@@ -69,9 +69,9 @@ import io.odysz.transact.x.TransException;
  * To use stamp in this way, nyquence numbers shold synchronized
  * in cross-domain style;
  * To use stamps for each domain, multiple change-logs for each
- * domain of an entity changing operation must be generated. 
+ * domain of an entity changing operation must be generated.
  * </pre>
- * 
+ *
  * @author Ody
  */
 public class DBSyntableBuilder extends DATranscxt {
