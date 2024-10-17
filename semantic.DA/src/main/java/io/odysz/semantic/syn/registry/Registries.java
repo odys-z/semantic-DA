0b9bd154b9767e2214b5f05b16c80d3eca29ebeb
Ody Zhou <odys.zhou@gmail.com>
2024-10-17 16:34:27 -0400

    fix test after separating Synmatics from DBSyntableBuilder, using DBSynTransBuilder for syn-change.

---
+++ semantic.DA/src/main/java/io/odysz/semantic/syn/registry/Registries.java
@@ -0,0 +1,11 @@
+package io.odysz.semantic.syn.registry;
+
+import java.util.ArrayList;
+
+import io.odysz.anson.Anson;
+
+public class Registries extends Anson {
+	
+	ArrayList<SyntityRegistry> registries;
+
+}
---
+++ semantic.DA/src/main/java/io/odysz/semantic/syn/registry/SyntityRegistry.java
@@ -0,0 +1,9 @@
+package io.odysz.semantic.syn.registry;
+
+import io.odysz.anson.Anson;
+
+public class SyntityRegistry extends Anson {
+
+	public String name;
+
+}
