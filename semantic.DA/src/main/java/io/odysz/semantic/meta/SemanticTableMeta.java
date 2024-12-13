5760ea480c87b5b1ec05ff5dd772a34f9fae2f83
Ody Z <odys.zhou@gmail.com>
2024-12-13 13:55:30 +0800

    Merging by steps of trying remove oz_autoseq.sql scripts.

--- semantic.DA/src/main/java/io/odysz/semantic/meta/SemanticTableMeta.java
+++ semantic.DA/src/main/java/io/odysz/semantic/meta/SemanticTableMeta.java
@@ -2,9 +2,17 @@ package io.odysz.semantic.meta;
 
 import static io.odysz.common.LangExt.isblank;
 import static io.odysz.common.LangExt.isNull;
-import static io.odysz.common.LangExt.eq;
+import static io.odysz.common.LangExt.len;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 import io.odysz.common.Utils;
 import io.odysz.module.xtable.XMLTable.IMapValue;
 import io.odysz.semantic.DASemantics;
@@ -14,6 +22,35 @@ import io.odysz.semantics.meta.TableMeta;
 import io.odysz.transact.x.TransException;
 
 public abstract class SemanticTableMeta extends TableMeta implements IMapValue {
+
+	protected static String loadSqlite(Class<?> clzz, String filename) {
+		try {
+			// https://stackoverflow.com/a/46468788/7362888
+			URI uri = Paths.get(clzz.getResource(filename).toURI()).toUri();
+//			if (zipfs == null)
+//				try {
+//					Map<String, String> env = new HashMap<>(); 
+//					env.put("create", "true");
+//					zipfs = FileSystems.newFileSystem(uri, env);
+//				}
+//				catch (Exception e) {
+//					Utils.warnT(new Object() {},
+//						"File %s shouldn't be load in the runtime environment.\ntarget URI: %s",
+//						filename, uri);
+//					e.printStackTrace();
+//					return null;
+//				}
+
+			return Files.readAllLines(
+				Paths.get(uri), Charset.defaultCharset())
+				.stream().collect(Collectors.joining("\n"));
+		} catch (URISyntaxException | IOException e) {
+			Utils.warnT(new Object() {},
+				"File %s can't be loaded in the runtime environment.\n%s",
+				filename, e.getMessage());
+			return null;
+		}
+	}
 
 	@Override
 	public String mapKey() {
@@ -35,12 +72,16 @@ public abstract class SemanticTableMeta extends TableMeta implements IMapValue {
 	@SuppressWarnings("unchecked")
 	public <T extends SemanticTableMeta> T replace() throws TransException, SQLException {
 		TableMeta mdb = Connects.getMeta(conn, tbl);
+		/** 2024.12.12 Tolerating multiple calling is the correct design?
 		if (debug && mdb instanceof SemanticTableMeta
 			&& (Connects.getDebug(conn) || !eq(mdb.getClass().getName(), getClass().getName())))
 			Utils.warn( "[TableMeta.debug true] Replacing existing Semantic table meta with new meta. Old: %s, new %s",
 						mdb.getClass().getName(), getClass().getName());
+		*/
 
-		DASemantics.replaceMeta(tbl, this, conn);
+		if (len(this.ftypes) == 0)
+			DASemantics.replaceMeta(tbl, this, conn);
+
 		if (isNull(this.ftypes) && mdb.ftypes() != null)
 			this.ftypes = mdb.ftypes();
 		return (T) this;
