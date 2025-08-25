bc16c29e52134a0e2797b70edd2a8238c74d8941
Odys Zhou <odys.zhou@gmail.com>
2025-08-25 11:48:58 +0800

    .

--- semantic.DA/src/main/java/io/odysz/semantic/meta/SemanticTableMeta.java
+++ semantic.DA/src/main/java/io/odysz/semantic/meta/SemanticTableMeta.java
@@ -12,7 +12,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
